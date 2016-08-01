902f2aa94339d66a89e7fca470471c501cbcf4ef
aless <aless@DESKTOP-98BHLOR.homenet.telecomitalia.it>
2016-08-01 19:11:02 +0200

    Prima prova Tess4J

---
+++ src/example/TesseractExample.java
@@ -0,0 +1,24 @@
+package example;
+
+import java.io.File;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
+public class TesseractExample {
+    
+    public static void main(String[] args) {
+        
+        File imageFile = new File("file_example.pdf");
+        Tesseract instance = Tesseract.getInstance(); // JNA Interface Mapping
+        // Tesseract1 instance = new Tesseract1(); // JNA Direct Mapping
+        // instance.setDatapath("<parentPath>"); // replace <parentPath> with path to parent directory of tessdata
+        // instance.setLanguage("eng");
+        
+        try {
+            String result = instance.doOCR(imageFile);
+            System.out.println(result);
+        } catch (TesseractException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
