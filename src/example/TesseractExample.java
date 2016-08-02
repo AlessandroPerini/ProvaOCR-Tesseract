d2b2695cbd4f2dd348fe90ef8d321e718279406d
aless <aless@Eeebook-Sara.homenet.telecomitalia.it>
2016-08-02 19:10:15 +0200

    Riordino file e librerie

--- src/example/TesseractExample.java
+++ src/example/TesseractExample.java
@@ -8,7 +8,7 @@ public class TesseractExample {
     
     public static void main(String[] args) {
         
-        File imageFile = new File("file_example.pdf");
+        File imageFile = new File("src/file/file_example.pdf");
         Tesseract instance = Tesseract.getInstance(); // JNA Interface Mapping
         // Tesseract1 instance = new Tesseract1(); // JNA Direct Mapping
         // instance.setDatapath("<parentPath>"); // replace <parentPath> with path to parent directory of tessdata
@@ -16,7 +16,7 @@ public class TesseractExample {
         
         try {
             String result = instance.doOCR(imageFile);
-            System.out.println(result);
+            System.out.println("\nRisultato della conversione:\n\n"+result);
         } catch (TesseractException e) {
             System.err.println(e.getMessage());
         }
