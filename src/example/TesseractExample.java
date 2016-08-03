0d1054c4972a15beacc5308a300f48e97aeabf2f
aless <aless@Eeebook-Sara.homenet.telecomitalia.it>
2016-08-03 22:28:55 +0200

    Riordinamento

--- src/example/TesseractExample.java
+++ src/example/TesseractExample.java
@@ -8,15 +8,17 @@ public class TesseractExample {
     
     public static void main(String[] args) {
         
-        File imageFile = new File("src/file/file_example.pdf");
+        String file = "src/file/file_example.pdf";
+        File imageFile = new File(file);
         Tesseract instance = Tesseract.getInstance(); // JNA Interface Mapping
         // Tesseract1 instance = new Tesseract1(); // JNA Direct Mapping
         // instance.setDatapath("<parentPath>"); // replace <parentPath> with path to parent directory of tessdata
         // instance.setLanguage("eng");
         
         try {
+            System.out.println("Processing...\n");
             String result = instance.doOCR(imageFile);
-            System.out.println("\nRisultato della conversione:\n\n"+result);
+            System.out.println("\nRisultato della conversione del file "+file+" :\n\n"+result);
         } catch (TesseractException e) {
             System.err.println(e.getMessage());
         }
