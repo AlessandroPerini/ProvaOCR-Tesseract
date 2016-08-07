f74b33486123f055e07449d935c281816576c5ae
aless <aless@Eeebook-Sara.homenet.telecomitalia.it>
2016-08-07 11:19:12 +0200

    Aggiunti file di riferimento lingua italiana

--- src/example/TesseractExample.java
+++ src/example/TesseractExample.java
@@ -1,24 +1,29 @@
 package example;
 
 import java.io.File;
-import net.sourceforge.tess4j.Tesseract;
-import net.sourceforge.tess4j.TesseractException;
+import tess4j.Tesseract;
+import tess4j.TesseractException;
 
 public class TesseractExample {
     
     public static void main(String[] args) {
         
-        String file = "src/file/file_example.pdf";
+        String file = "src/file/image1.jpg";
         File imageFile = new File(file);
-        Tesseract instance = Tesseract.getInstance(); // JNA Interface Mapping
-        // Tesseract1 instance = new Tesseract1(); // JNA Direct Mapping
-        // instance.setDatapath("<parentPath>"); // replace <parentPath> with path to parent directory of tessdata
-        // instance.setLanguage("eng");
+        Tesseract instance = Tesseract.getInstance(); 
+        
+        /* Tesseract1 instance = new Tesseract1();
+           instance.setDatapath("<parentPath>");
+           instance.setLanguage("eng"); */
         
         try {
             System.out.println("Processing...\n");
+            long inizio = System.currentTimeMillis();
             String result = instance.doOCR(imageFile);
             System.out.println("\nRisultato della conversione del file "+file+" :\n\n"+result);
+            long fine = System.currentTimeMillis(); 
+            long tempo = (long) ((fine - inizio) * 0.001);
+            System.out.println("Tempo impegato: " + tempo + " secondi\n\n");
         } catch (TesseractException e) {
             System.err.println(e.getMessage());
         }
